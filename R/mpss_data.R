b1a3a9d4808ffe9e1f57dbd8ee373ec02dc92bc6
Ernest Guevarra <ernest@guevarra.io>
2025-05-23 12:02:21 +0100

    add plots to michigan case study

---
+++ R/mpss_data.R
@@ -0,0 +1,10 @@
+#'
+#' Read and process MPSS Fall 2011 dataset
+#' 
+
+mpss_process_data <- function() {
+  df <- read.csv("data/f11data-use_pud.csv")
+
+  
+}
+
--- packages.R
+++ packages.R
@@ -1,7 +1,17 @@
 # Load R package dependencies --------------------------------------------------
 
-## Packags for publishing ----
+## Packages for publishing ----
 library(quarto)
 library(knitr)
 library(rmarkdown)
 library(yaml)
+
+
+## Packages for working with data ----
+library(dplyr)
+library(ggplot2)
+
+
+## Packages for colour palettes ----
+library(oxthema)
+
